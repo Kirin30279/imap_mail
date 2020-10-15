b86b17224f07f3b67f3835d4141190986b741e4c
DESKTOP-KLJVRMO\bibianuser <andy860826@japan-import.com.tw>
2020-10-15 14:21:55 +0800

    整理內容

--- main_saveMailtoDB.php
+++ main_saveMailtoDB.php
@@ -1,65 +1,39 @@
 <?php
 require __DIR__ . '/vendor/autoload.php';
-
+include 'src/saveFunction.php';
+include 'LoginInfo.php';
 use Ddeboer\Imap\Server;
+echo time();//測試用，正式可刪
 
-// $server = new Server('imap.gmail.com');
-$server = new Server('export.imap.mail.yahoo.com');
-include 'DBinfo.php';
-// $connection is instance of \Ddeboer\Imap\Connection
-// $connection = $server->authenticate('bibian.IMAP2020@gmail.com', 'bibian12345');
-$connection = $server->authenticate('andy860826@yahoo.com.tw', 'rcfftxtbiwgjrjkf');
+$server = new Server('pop.mail.yahoo.com');
+$connection = $server->authenticate($username, $password);
 
-// $mailbox = $connection->getMailbox('INBOX');//只讀主收件匣，垃圾桶等等不管
-$mailbox = $connection->getMailbox('Inbox');
+$mailbox = $connection->getMailbox('INBOX');//只讀主收件匣
 $messages = $mailbox->getMessages();
 
-$count_seen = 0;
-// echo $connection->count();
-// exit();
+$count_seen = 0;//計算本次的信，正式可刪
 
 foreach ($messages as $message) {
     if($message->isSeen()){
         $count_seen += 1;
     } else{
         // $message->setFlag('\\Seen');
-
-        $message_id = htmlspecialchars($message->getId());
-        $from_who = htmlspecialchars($message->getFrom()->getName());
-        $from_address = htmlspecialchars($message->getFrom()->getAddress());
-        $title = htmlspecialchars ($message->getSubject());
-        $time = date('Y-m-d H:i:s',$message->getDate()->getTimestamp());
-        $text = htmlspecialchars ($message->getBodyHtml());
+        $dataArray = getDataFromMailbox($message);
+        $dataArray = handleSpecialChar($dataArray);
         
+        /*
+        * saveAttachmentReturnPath是存附件，存完回傳路徑(type:String)
+        * 可再想想如何拆，一個函數做一件事情就好
+        */
+        $file_path = saveAttachmentReturnPath($message);
 
-        if($message->getAttachments()!=array()){//存附件
-            $file_path = array();
-            $attachments = $message->getAttachments();
-            foreach ($attachments as $attachment) {
-                //如果要限制大小就用getSize來限制
-                $save_path = '../pic/'.time()."_".$attachment->getFilename();
-                file_put_contents(
-                    $save_path,
-                    $attachment->getDecodedContent()
-                );
-                $file_path[] = $save_path;
-            }   
-            $file_path=implode(",",$file_path);
-        } else{
-            $file_path = '';
-        }
-      
-
-        $insertSQL_mail = "INSERT INTO mail(`message_ID`, `from_who`, `from_address`, `title`, `time`, `message_text`, `message_file_location`) 
-        VALUES ('$message_id', '$from_who', '$from_address', '$title', '$time', '$text', '$file_path');";        
-        
-        $DB_Connect->query($insertSQL_mail);
-
-        echo '<br>'."=============大分隔線-區分不同封信件=============".'<br>';
+        insertDataToDB($dataArray, $file_path);
+        echo '<br>'."=============大分隔線-區分不同封信件=============".'<br>';//正式要刪
     }
 
 }
 
-
 echo "收件匣中有".$mailbox->count()."封信".'<br>';
 echo "其中有".$count_seen."封信已經讀過，並未存到資料庫中。".'<br>';
+
+echo time();//測試用，正式可刪
